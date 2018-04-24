5b9d07459a2f0c89f6c2604ad6ed76e51efe7210
Angel <angel.r.ruiz@gmail.com>
2018-04-24 14:21:34 -0500

    "Initial layout"

---
+++ VW2018/ViewControllerCrafter.swift
@@ -0,0 +1,39 @@
+//
+//  ViewControllerCrafter.swift
+//  VW2018
+//
+//  Created by Alumno on 24/04/18.
+//  Copyright © 2018 Gekko. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerCrafter: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func selectCrafter(_ sender: Any) {
+        self.performSegue(withIdentifier: "toTabBar", sender: self)
+    }
+    
+}
